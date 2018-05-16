084b2bb14bed5d866ff41b03487d300e035c7c55
Ray <ray247k@gmail.com>
2018-05-16 17:10:30 +0800

    Add: form

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,7 +1,20 @@
 @extends('layouts.master')
 
 @section('content')
-    <div>
-        吵架內容
-    </div>
+    <main role="main">
+        <h1>發表文章</h1>
+        <form method="post" action="/posts">
+            {{ csrf_field() }}
+            <div class="form-group">
+                <label for="body">內文</label>
+                <textarea type="text" class="form-control" id="body" name="body" placeholder="文章內文"></textarea>
+            </div>
+            <div class="form-group">
+                <button type="submit" class="btn btn-primary">送出</button>
+            </div>
+        </form>
+
+        @include('layouts.errors')
+
+    </main>
 @endsection
